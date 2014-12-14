87390e92c9b224c6299ebf9b68bdc815007f3bc5
robbert <robbert@vdzon.com>
2014-12-14 18:56:10 +0100

    eerste versie

--- resources/linux-full-install.sh
+++ resources/linux-full-install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# install git and clone the sources and scripts
+apt-get install git
+mkdir /workspace
+chmod a+rwx /workspace
+cd /workspace
+git clone https://github.com/robbertvdzon/contactdb.v1.git
+
+
+#update apt
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
 
+#install tools for silent installations
 apt-get install debconf-utils
 
 echo "mysql-server-5.5 mysql-server/root_password_again password admin" | debconf-set-selections

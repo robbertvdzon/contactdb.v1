39416c6326bee1aa1d96b0ae648567deb7c0676c
robbert <robbert@vdzon.com>
2015-03-14 16:42:31 +0100

    update

--- docker/mysql/config.sh
+++ docker/mysql/config.sh
@@ -6,3 +6,4 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;"
 #mysql -u root --password=admin < /data/contact.sql
 mysql -u root < /data/contact.sql
+sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf

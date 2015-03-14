4c541c8d435ddae44b990994741c8d7e32e8dbdb
robbert <robbert@vdzon.com>
2015-03-14 10:19:42 +0100

    sql changes

--- docker/mysql/config.sh
+++ docker/mysql/config.sh
@@ -1,8 +1,6 @@
-FROM wnameless/mysql-phpmyadmin
-COPY contact.sql /data/contact.sql
-RUN mysql -u root --password=admin < /data/contact.sql
-
-EXPOSE 3306
-
-CMD mysqld
-
+#/bin/bash
+mysqld_safe &
+mysqladmin --silent --wait=30 ping 
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+#mysql -u root --password=admin < /data/contact.sql
+mysql -u root < /data/contact.sql

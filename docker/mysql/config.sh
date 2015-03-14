6a7170844a3e42968ab1d34c7dd50dfb1847a0d8
robbert <robbert@vdzon.com>
2015-03-14 14:38:41 +0100

    update

--- docker/mysql/config.sh
+++ docker/mysql/config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "mysqld: ALL" >> /etc/hosts.allow
 mysqld_safe &
 mysqladmin --silent --wait=30 ping 
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;"
 #mysql -u root --password=admin < /data/contact.sql
 mysql -u root < /data/contact.sql

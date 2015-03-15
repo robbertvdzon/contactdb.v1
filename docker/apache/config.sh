15d75dcea15f8d9554703655fa186f8335b62b9b
robbert <robbert@vdzon.com>
2015-03-15 21:38:23 +0100

    update

--- docker/apache/config.sh
+++ docker/apache/config.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/default.conf
-mv /etc/apache2/sites-available/default.conf /etc/apache2/sites-available/000-default.conf
+rm -f /tmp/add.txt
 echo 'ProxyPass /api http://wildfly:8080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
 echo 'ProxyPassReverse /api http://wildfly:8080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
-sed  -i '/<VirtualHost/r /tmp/add.txt' /etc/apache2/sites-available/default
-rm -f /tmp/add.txt
-
-
-mkdir /var/lock/apache2
-
-
-mkdir -p /data/app
-rm -fr /var/www/html
-ln -s /data/app /var/www/html
+sed  -i '/<VirtualHost/r /tmp/add.txt' /etc/apache2/sites-available/000-default.conf
+a2enmod proxy proxy_http
--- docker/apache/run.sh
+++ docker/apache/run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-rm -f /tmp/add.txt
-echo 'ProxyPass /api http://wildfly:8080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
-echo 'ProxyPassReverse /api http://wildfly:8080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
-sed  -i '/<VirtualHost/r /tmp/add.txt' /etc/apache2/sites-available/000-default.conf
-
-a2enmod proxy proxy_http
-
 chown www-data:www-data /app -R
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND

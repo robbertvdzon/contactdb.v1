aff3ac0701621cca03be285c02579fd9bfb8a45b
robbert <robbert@vdzon.com>
2015-03-15 21:36:02 +0100

    update

--- docker/apache/run.sh
+++ docker/apache/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -f /tmp/add.txt
-echo 'ProxyPass /api http://wildfly:28080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
-echo 'ProxyPassReverse /api http://wildfly:28080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
+echo 'ProxyPass /api http://wildfly:8080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
+echo 'ProxyPassReverse /api http://wildfly:8080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
 sed  -i '/<VirtualHost/r /tmp/add.txt' /etc/apache2/sites-available/000-default.conf
 
 a2enmod proxy proxy_http

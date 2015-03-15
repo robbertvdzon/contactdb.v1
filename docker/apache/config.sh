0d4a906102a73072f1b3a64f5dfed2e3c99b1dea
robbert <robbert@vdzon.com>
2015-03-15 20:31:26 +0100

    update

--- docker/apache/config.sh
+++ docker/apache/config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/default.conf
+mv /etc/apache2/sites-available/default.conf /etc/apache2/sites-available/000-default.conf
 echo 'ProxyPass /api http://wildfly:28080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
 echo 'ProxyPassReverse /api http://wildfly:28080/contactsbackend-1.0-SNAPSHOT' >> /tmp/add.txt
 sed  -i '/<VirtualHost/r /tmp/add.txt' /etc/apache2/sites-available/default

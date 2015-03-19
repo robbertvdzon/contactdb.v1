ada29818361a5fd9ec6fd7fd6e804511388703fa
robbert <robbert@vdzon.com>
2015-03-19 20:45:42 +0100

    update

---
+++ install_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker rm -f apache
+docker rm -f wildfly
+docker rm -f mysql
+docker rm -f datavolume
+docker build -t robbertvdzon/datavolume /workspace/contactdb.v1/docker/datavolume
+docker run -d --name datavolume robbertvdzon/datavolume
+docker build -t robbertvdzon/mysql /workspace/contactdb.v1/docker/mysql
+docker run -d -p 4022:22 -p 888:80 --name mysqldb --volumes-from datavolume robbertvdzon/mysql
+docker build -t robbertvdzon/wildfly /workspace/contactdb.v1/docker/wildfly
+docker run -d -it -p 8787:8787 -p 9990:9990 -p 8080:8080 --name wildfly --volumes-from datavolume --link mysqldb:mysqldb robbertvdzon/wildfly
+docker build -t robbertvdzon/apache /workspace/contactdb.v1/docker/apache
+docker run -d -it -p 80:80 --name apache --volumes-from datavolume --link wildfly:wildfly robbertvdzon/apache

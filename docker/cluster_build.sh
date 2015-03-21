dcc82dd0e775f4e73fa8d7d8e034fa98ebc5105c
robbert <robbert@vdzon.com>
2015-03-21 19:37:49 +0100

    update

--- docker/cluster_build.sh
+++ docker/cluster_build.sh
@@ -28,13 +28,13 @@ docker rm -f $DOCKERNAME_WILDFLY
 docker rm -f $DOCKERNAME_MYSQLDB
 
 # build and run mysql container
-docker build -t robbertvdzon/$DOCKERNAME_MYSQLDB ./docker/mysql
+docker build -t robbertvdzon/$DOCKERNAME_MYSQLDB ./mysql
 docker run -d -p $MYSQL_SSH_PORT:22 -p $MYPHPADMIN_PORT:80 --name $DOCKERNAME_MYSQLDB robbertvdzon/$DOCKERNAME_MYSQLDB
 
 # build and run wildfly container
-docker build -t robbertvdzon/$DOCKERNAME_WILDFLY ./docker/wildfly
+docker build -t robbertvdzon/$DOCKERNAME_WILDFLY ./wildfly
 docker run -d -it -p $WILDFLY_SSH_PORT:22 -p $WILDFLY_DEBUG_PORT:8787 -p $WILDFLY_ADMIN_PORT:9990 -p $WILDFLY_APP_PORT:8080 --name $DOCKERNAME_WILDFLY --link $DOCKERNAME_MYSQLDB:mysqldb robbertvdzon/$DOCKERNAME_WILDFLY
 
 # build and run apache container
-docker build -t robbertvdzon/$DOCKERNAME_APACHE ./docker/apache
+docker build -t robbertvdzon/$DOCKERNAME_APACHE ./apache
 docker run -d -it -p $APACHE_SSH_PORT:22 -p $APACHE_PORT:80 --name $DOCKERNAME_APACHE --link $DOCKERNAME_WILDFLY:wildfly robbertvdzon/$DOCKERNAME_APACHE

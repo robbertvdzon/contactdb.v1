21eee8c872805ac045134126777efe32a680921d
robbert <robbert@vdzon.com>
2015-03-22 09:23:32 +0100

    update

--- docker/apache/config.sh
+++ docker/apache/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f /tmp/add.txt
-echo 'ProxyPass /resources http://wildfly:8080/contactsbackend-1.0-SNAPSHOT/resources' >> /tmp/add.txt
-echo 'ProxyPassReverse /resources http://wildfly:8080/contactsbackend-1.0-SNAPSHOT/resources' >> /tmp/add.txt
+echo 'ProxyPass /resources http://wildfly:8080/api/resources' >> /tmp/add.txt
+echo 'ProxyPassReverse /resources http://wildfly:8080/api/resources' >> /tmp/add.txt
 sed  -i '/<VirtualHost/r /tmp/add.txt' /etc/apache2/sites-available/000-default.conf
 a2enmod proxy proxy_http

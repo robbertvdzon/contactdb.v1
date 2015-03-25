b84433e25c78ee2517c66f992ac7e8bc6026d02e
robbert <robbert@vdzon.com>
2015-03-25 20:24:50 +0100

    update

--- src/main/java/com/vdzon/samples/uselesscontacts/rest/AuthenticationResource.java
+++ src/main/java/com/vdzon/samples/uselesscontacts/rest/AuthenticationResource.java
@@ -12,7 +12,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 
-//@DeclareRoles({"root", "user","anonymous" })
 @ApplicationPath("/resources")
 @Path("/auth")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,20 +25,6 @@ public class AuthenticationResource {
     @Path("login")
     @PermitAll
     public AuthAccessElement login(@Context HttpServletRequest request, AuthLoginElement loginElement) {
-
-        //TODO: DIT MOET NATUURLIJK WEG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //TODO: DIT MOET NATUURLIJK WEG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        boolean autoLogin = true;//TODO: DIT MOET NATUURLIJK WEG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        if (autoLogin){
-            request.getSession().setAttribute(AuthAccessElement.PARAM_AUTH_ID, "1");
-            request.getSession().setAttribute(AuthAccessElement.PARAM_AUTH_TOKEN, "1");
-            AuthAccessElement el = new AuthAccessElement();
-            el.setAuthId(1);
-            el.setAuthPermission("root");
-            el.setAuthToken("1");
-            return el;
-        }
-
         AuthAccessElement accessElement = authService.login(loginElement);
         if (accessElement != null) {
             request.getSession().setAttribute(AuthAccessElement.PARAM_AUTH_ID, accessElement.getAuthId());
